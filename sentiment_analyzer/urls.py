f6dc84b449d295af6d564a1b5600eb86944d6bca
priyanshu3103 <priyanshubarapatre19@gmail.com>
2024-02-04 15:44:36 +0530

    modified

--- sentiment_analyzer/urls.py
+++ sentiment_analyzer/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from sentiment import views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('classify/', views.call_model.as_view()),
+    path('streamlit/', TemplateView.as_view(template_name='streamlit_app.html')),
    
     
 ]
